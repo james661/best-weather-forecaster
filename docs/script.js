58d5b86fca899e65281c700dcff655bfdc63562e
A Friend <jamesshami13@gmail.com>
2023-06-06 23:46:24 -0700

    Added comments and some CSS

--- docs/script.js
+++ docs/script.js
@@ -1,12 +1,13 @@
+// Puts my API key into a set variable for easy access later
 const apiKey = "ee8203485231b4b5de602e6ff44459bc";
-
-document.querySelector("form").addEventListener("submit", function (e) {
-  e.preventDefault();
+// Begins a function when submitting that grabs the values of a city if typed in
+document.querySelector("form").addEventListener("submit", function (sub) {
+  sub.preventDefault();
   const city = document.getElementById("cityInput").value;
   getCurrentWeather(city);
   getForecast(city);
 });
-
+// Function that fetches the current weather data for the searched city
 function getCurrentWeather(city) {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
@@ -23,7 +24,7 @@ function getCurrentWeather(city) {
       console.error(error);
     });
 }
-
+// Function that fetches the weather data for the next 5 days.
 function getForecast(city) {
   const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`;
 
@@ -37,6 +38,7 @@ function getForecast(city) {
       const dailyForecasts = groupForecastsByDay(forecastList);
 
       // Display maximum temperature, humidity, and wind speed for each day
+      let dayCount = 0;
       for (const [day, forecasts] of Object.entries(dailyForecasts)) {
         const maxTemperature = Math.max(
           ...forecasts.map((forecast) => forecast.main.temp)
@@ -47,23 +49,30 @@ function getForecast(city) {
         const maxWindSpeed = Math.max(
           ...forecasts.map((forecast) => forecast.wind.speed)
         );
-
+        // Displays the weather data for each day
         forecastHTML += `<p>${day}: Temperature: ${maxTemperature} ºF, Humidity: ${maxHumidity}%, Wind Speed: ${maxWindSpeed} mph</p>`;
+        // Halts the function so no more than 5 days are shown
+        dayCount++;
+        if (dayCount >= 5) {
+          break;
+        }
       }
 
       document.getElementById("forecast").innerHTML = forecastHTML;
     })
+    // Shows an error if the API doesn't load properly
     .catch((error) => {
       console.log("Error:", error);
     });
+  // Saves each searched city in local storage, using a function coded below
   saveSearchedCity(city);
 }
-
+// Function that groups forecast items based on their dates
 function groupForecastsByDay(forecastList) {
   const dailyForecasts = {};
 
   for (const forecast of forecastList) {
-    const date = dayjs(forecast.dt_txt).format("YYYY-MM-DD");
+    const date = dayjs(forecast.dt_txt).format("MMMM-DD-YYYY");
     if (!dailyForecasts[date]) {
       dailyForecasts[date] = [];
     }
@@ -72,7 +81,7 @@ function groupForecastsByDay(forecastList) {
 
   return dailyForecasts;
 }
-
+// Function that saves cities that have been searched
 function saveSearchedCity(city) {
   let searchedCities = localStorage.getItem("searchedCities");
   if (searchedCities) {
@@ -88,11 +97,11 @@ function saveSearchedCity(city) {
 
   displaySearchedCities(searchedCities);
 }
-
+// Function that displays the previously searched cities
 function displaySearchedCities(searchedCities) {
   const searchedCitiesList = document.getElementById("searchedCities");
   searchedCitiesList.innerHTML = "";
-
+  // Creates a list for the previously searched cities
   for (const city of searchedCities) {
     const li = document.createElement("li");
     li.textContent = city;
